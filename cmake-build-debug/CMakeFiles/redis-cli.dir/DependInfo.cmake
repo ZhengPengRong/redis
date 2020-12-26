64c92f50df044f7f4af129ee9c46ca22ba5d08f0
lihaoran <lihaoran@totok.ai>
2020-12-26 17:39:04 +0400

    add CMake files

---
+++ cmake-build-debug/CMakeFiles/redis-cli.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/haoran.li/Documents/GitHub/redis/src/adlist.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/adlist.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/src/ae.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/ae.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/src/anet.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/anet.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/src/crc64.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/crc64.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/src/redis-cli.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/redis-cli.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/src/release.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/release.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/src/sds.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/sds.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/src/zmalloc.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/zmalloc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../deps/hiredis"
+  "../deps/linenoise"
+  "../deps/lua/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/linenoise/CMakeFiles/linenoise.dir/DependInfo.cmake"
+  "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/deps/lua/CMakeFiles/lua.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/fpconv.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/fpconv.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lapi.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lapi.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lauxlib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lauxlib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lbaselib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lbaselib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lcode.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lcode.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/ldblib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/ldblib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/ldebug.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/ldebug.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/ldo.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/ldo.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/ldump.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/ldump.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lfunc.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lfunc.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lgc.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lgc.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/linit.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/linit.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/liolib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/liolib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/llex.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/llex.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lmathlib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lmathlib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lmem.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lmem.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/loadlib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/loadlib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lobject.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lobject.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lopcodes.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lopcodes.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/loslib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/loslib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lparser.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lparser.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lstate.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lstate.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lstring.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lstring.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lstrlib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lstrlib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/ltable.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/ltable.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/ltablib.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/ltablib.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/ltm.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/ltm.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lua.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lua.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lua_bit.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lua_bit.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lua_cjson.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lua_cjson.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lua_cmsgpack.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lua_cmsgpack.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lua_struct.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lua_struct.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/luac.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/luac.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lundump.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lundump.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lvm.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lvm.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/lzio.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/lzio.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/print.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/print.c.o"
+  "/Users/haoran.li/Documents/GitHub/redis/deps/lua/src/strbuf.c" "/Users/haoran.li/Documents/GitHub/redis/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/src/strbuf.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
